7d65d728cadb58a4b5cd88d2d41851cf4dbe44ae
kzwin <kz@example.org>
2017-07-18 06:14:36 -0400

    good wip

--- main.go
+++ main.go
@@ -28,6 +28,10 @@ func main() {
 	sameFiles := make(map[sameFile][]string)
 	for _, dir := range flag.Args() {
 		filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+			if f == nil {
+				fmt.Printf("Invalid path %s\n", path)
+				return nil
+			}
 			if !f.IsDir() {
 				s := sameFile{f.Name(), f.Size()}
 				if s.size == 0 {
@@ -38,37 +42,50 @@ func main() {
 			return nil
 		})
 	}
+	total_dupes := 0
+	var size_wasted int64
 	for k, v := range sameFiles {
 		if k.size == 0 || len(v) < 2 {
 			continue
 		}
-		fingerPrint := make(map[string]int)
-		toDelete := []string{}
-		fmt.Printf("%s %d ==> %d\n", k.name, k.size, len(v))
+		fingerPrint := make(map[string][]string)
 		for _, path := range v {
-			fmt.Printf("\t%s\n", path)
 			f, err := os.Open(path)
 			if err != nil {
 				log.Fatal(err)
 			}
-			defer f.Close()
 			h := md5.New()
 
 			if _, err := io.Copy(h, f); err != nil {
 				log.Fatal(err)
 			}
+			f.Close()
 			md5 := string(h.Sum(nil)[:])
-			fmt.Printf("%x\n", md5)
-			fingerPrint[md5]++
-      fmt.Printf("has prefix %s %v\n", path, strings.HasPrefix(path, *deletePrefix))
-			if *deletePrefix != "" && strings.HasPrefix(path, *deletePrefix) {
-				toDelete = append(toDelete, path)
-			}
+			fingerPrint[md5] = append(fingerPrint[md5], path)
 		}
-		if 1 < len(fingerPrint) {
-			for _, filePath := range toDelete {
-				fmt.Printf("going to delete %v\n", filePath)
+		for _, paths := range fingerPrint {
+			toDelete := []string{}
+			if 2 > len(paths) {
+				continue
+			}
+			total_dupes--
+			size_wasted = size_wasted - k.size
+			for i, p := range paths {
+				total_dupes++
+				size_wasted = size_wasted + k.size
+				fmt.Printf("duplicate %d: %s\n", i, p)
+				if *deletePrefix != "" && strings.HasPrefix(p, *deletePrefix) {
+					toDelete = append(toDelete, p)
+				}
+			}
+			if len(toDelete) == len(paths) {
+				fmt.Println("delete prefix needs to be more restrictive.  all copies of a file are")
+				for _, p := range toDelete {
+					fmt.Printf("\t%s\n", p)
+				}
+				continue
 			}
 		}
 	}
+	fmt.Printf("\nTotal dupes %d.  Total bytes wasted %d\n", total_dupes, size_wasted)
 }
